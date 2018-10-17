4126170b56277155a40a2cdff323594db91cdac1
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-10-17 13:16:05 +0100

    edits

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,50 +1,10 @@
 $(document).ready(function(){
-  // let res = startGame();
-  var player1 = new Player(true, 'X');
-  var player2 = new Player(false, 'O');
 
-  var numbers = [1,2,3,4,5,6,7,8,9]
+  $(".container").hide();
 
-  for (num of numbers) {
 
-    $(`#${num}`).click(function(e){
 
-      function removePick(numIndex, arrLength) {
-          if (numIndex > -1) numbers.splice(numIndex, 1);
-          if (numbers.length < arrLength) return true;
-      }
 
-      if(player1.turn){
-
-        $(this).addClass("clickedRed");
-        let id = parseInt(e.currentTarget.id);
-        let index = numbers.indexOf(id);
-        let numArrLen = numbers.length;
-        let pick = removePick(index, numArrLen);
-        player1.select(id);
-        player1.switch();
-        player2.switch();
-        // console.log(player1.selected);
-        player1.checkWin()
-        // console.log(player1.won);
-      } else {
-
-        $(this).addClass("clickedGreen");
-        let id = parseInt(e.currentTarget.id);
-        let index = numbers.indexOf(id);
-        let numArrLen = numbers.length;
-        let pick = removePick(index, numArrLen);
-        player2.select(id);
-        player2.switch();
-        player1.switch();
-        // console.log(player2.selected);
-        player2.checkWin()
-        // console.log(player2.won);
-      }
-
-    }) // .click
-
-  }
 });//doc ready
 
 
@@ -69,6 +29,9 @@ class Player {
 
     //winning sequences
     const winSequences = [ [1,2,3], [4,5,6], [7,8,9], [7,4,1], [8,5,2], [9,6,3], [7,5,3], [1,5,9] ];
+
+
+
 
     //loop through winSequences
     for (var seq = 0; seq < winSequences.length; seq++) {
@@ -118,3 +81,57 @@ class Player {
 
   }//checkWin
 }//player class
+
+
+
+function initGame(){
+  /* return true when finished */
+
+  //init players
+  var player1 = new Player(true, 'X');
+  var player2 = new Player(false, 'O');
+
+  //array holds numers
+  var numbers = [1,2,3,4,5,6,7,8,9]
+
+  for (num of numbers) {
+
+    $(`#${num}`).click(function(e){
+
+      function removePick(numIndex, arrLength) {
+          if (numIndex > -1) numbers.splice(numIndex, 1);
+          if (numbers.length < arrLength) return true;
+      }
+
+      if(player1.turn){
+
+        $(this).addClass("clickedRed");
+        let id = parseInt(e.currentTarget.id);
+        let index = numbers.indexOf(id);
+        let numArrLen = numbers.length;
+        let pick = removePick(index, numArrLen);
+        player1.select(id);
+        player1.switch();
+        player2.switch();
+        // console.log(player1.selected);
+        player1.checkWin()
+        // console.log(player1.won);
+      } else {
+
+        $(this).addClass("clickedGreen");
+        let id = parseInt(e.currentTarget.id);
+        let index = numbers.indexOf(id);
+        let numArrLen = numbers.length;
+        let pick = removePick(index, numArrLen);
+        player2.select(id);
+        player2.switch();
+        player1.switch();
+        // console.log(player2.selected);
+        player2.checkWin()
+        // console.log(player2.won);
+      }
+
+    }) // .click
+
+  }
+}
