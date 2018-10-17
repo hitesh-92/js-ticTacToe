e15fa2b1ac205f894f2d4b29ee71e7fbc11b6081
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-10-17 16:09:17 +0100

    added messages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,9 +2,81 @@ $(document).ready(function(){
 
   $(".container").hide();
 
+  $("#init-btn").click(function(){
+    $(this).fadeOut(500);
+    $(".container").fadeIn(1000);
+    $("#winMessaege").fadeOut()
 
+    //init players
+    var player1 = new Player(true, 'X');
+    var player2 = new Player(false, 'O');
 
+    //array holds numers
+    var numbers = [1,2,3,4,5,6,7,8,9]
 
+    for (num of numbers) {
+      $(`#${num}`).removeClass("clickedGreen");
+      $(`#${num}`).removeClass("clickedRed");
+
+      $(`#${num}`).click(function(e){
+
+        function removePick(numIndex, arrLength) {
+            if (numIndex > -1) numbers.splice(numIndex, 1);
+            if (numbers.length < arrLength) return true;
+        }
+
+        if(player1.turn){
+
+          $(this).addClass("clickedRed");
+          let id = parseInt(e.currentTarget.id);
+          let index = numbers.indexOf(id);
+          let numArrLen = numbers.length;
+          let pick = removePick(index, numArrLen);
+          player1.select(id);
+          player1.switch();
+          player2.switch();
+          // console.log(player1.selected);
+          player1.checkWin()
+          // console.log(player1.won);
+          if (player1.won){
+            console.log('p1 win');
+            $("#winMessaege").html("<i>Player1 has won!</i>")
+            $("#winMessaege").fadeIn()
+            $(".container").fadeOut()
+
+            $("#init-btn").fadeIn()
+            player1.reset()
+            player2.reset()
+            player1.turn = true;
+          }
+        } else {
+
+          $(this).addClass("clickedGreen");
+          let id = parseInt(e.currentTarget.id);
+          let index = numbers.indexOf(id);
+          let numArrLen = numbers.length;
+          let pick = removePick(index, numArrLen);
+          player2.select(id);
+          player2.switch();
+          player1.switch();
+          // console.log(player2.selected);
+          player2.checkWin()
+          // console.log(player2.won);
+          if (player2.won){
+            $("#winMessaege").html("<i>Player2 has won!</i>")
+            $("#winMessaege").fadeIn()
+            console.log('p2 win');
+            $(".container").fadeOut()
+
+            $("#init-btn").fadeIn()
+            player1.reset()
+            player2.reset()
+            player1.turn = true;
+          }
+        }
+      }) // .click
+    } //for num
+  });
 });//doc ready
 
 
@@ -24,14 +96,17 @@ class Player {
     this.selected.push(number);
   }
 
+  reset(){
+    this.turn = false;
+    this.selected = [];
+    this.won = false;
+  }
+
   checkWin(){
     let result = false;
 
     //winning sequences
     const winSequences = [ [1,2,3], [4,5,6], [7,8,9], [7,4,1], [8,5,2], [9,6,3], [7,5,3], [1,5,9] ];
-
-
-
 
     //loop through winSequences
     for (var seq = 0; seq < winSequences.length; seq++) {
@@ -55,83 +130,7 @@ class Player {
       }
     }
 
-
-
-    /* //node code
-    for (seq of winSequences){
-      let count = 0; // increment
-      //loop through individual sequence
-      for (each of seq){
-        //loop through player picks, this.seleted
-        for(pick of this.selected){
-          //if each matches pick, count++
-          if (pick == each) count++
-        }
-        //check if 3 matches made
-        if (count == 3) result = true
-      }
-    }
-    */
-
-
-
-    console.log('result', result);
     // change this.won status
     if(result) this.won = true;
-
   }//checkWin
 }//player class
-
-
-
-function initGame(){
-  /* return true when finished */
-
-  //init players
-  var player1 = new Player(true, 'X');
-  var player2 = new Player(false, 'O');
-
-  //array holds numers
-  var numbers = [1,2,3,4,5,6,7,8,9]
-
-  for (num of numbers) {
-
-    $(`#${num}`).click(function(e){
-
-      function removePick(numIndex, arrLength) {
-          if (numIndex > -1) numbers.splice(numIndex, 1);
-          if (numbers.length < arrLength) return true;
-      }
-
-      if(player1.turn){
-
-        $(this).addClass("clickedRed");
-        let id = parseInt(e.currentTarget.id);
-        let index = numbers.indexOf(id);
-        let numArrLen = numbers.length;
-        let pick = removePick(index, numArrLen);
-        player1.select(id);
-        player1.switch();
-        player2.switch();
-        // console.log(player1.selected);
-        player1.checkWin()
-        // console.log(player1.won);
-      } else {
-
-        $(this).addClass("clickedGreen");
-        let id = parseInt(e.currentTarget.id);
-        let index = numbers.indexOf(id);
-        let numArrLen = numbers.length;
-        let pick = removePick(index, numArrLen);
-        player2.select(id);
-        player2.switch();
-        player1.switch();
-        // console.log(player2.selected);
-        player2.checkWin()
-        // console.log(player2.won);
-      }
-
-    }) // .click
-
-  }
-}
