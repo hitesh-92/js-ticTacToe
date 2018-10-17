fd0a5b689fafead9f217c6b85256f9dd2e81d62e
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-10-17 22:04:15 +0100

    player class removeNumber refactor

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     var player1 = new Player(true, 'X');
     var player2 = new Player(false, 'O');
 
-    console.log('* INIT *',player1,player2);
+    // console.log('* INIT *',player1,player2);
 
     for (var num = 1; num <= 9; num++) {
       $(`#${num}`).removeClass("clickedGreen");
@@ -23,23 +23,25 @@ $(document).ready(function(){
         if(player1.turn){ //  * PLAYER 1 *
 
           let message = true;
-          $(this).addClass("clickedRed");
 
           //removeNumber deletes pick from array
           let picked = player1.removeNumber(e.currentTarget.id, true);
-          //if successful deletes from player2's array
-          if(picked) player2.removeNumber(id, false);
 
-          //checkWin returns true is 3 in a row
-          let check = player1.checkWin();
+          //if successful deletes from player2's array + color square
+          if(picked) {
+            player2.removeNumber(id, false);
+            $(this).addClass("clickedRed");
+          }
+
           //length of array, if 9 all moves made
           let count = player1.getLength();
 
           //tie and win message. message no longer true, will end game
-          if(count == 9 && !check){
+          if(count == 9 && player1.won == false){
             message = "Tie Break! No Winner This Time!"
-          } else if (check) {
-            message = "Player1 Has Won!!"
+          } else if (player1.won) {
+            message = "Player1 Has Won!!";
+            // console.log(message);
           }
 
           //end game
@@ -52,6 +54,9 @@ $(document).ready(function(){
 
           //switch players, continue game
           player1.switch(); player2.switch();
+
+          console.log('player1', '| picked',picked, '| count',count);
+
 
         } else { // * PLAYER 2 *
 
@@ -86,7 +91,7 @@ class Player {
     this.turn = turn;
     this.symbol = symbol,
     this.selected = [],
-    // this.won = false,
+    this.won = false,
     this.numbers = [1,2,3,4,5,6,7,8,9]
   }
 
@@ -99,53 +104,45 @@ class Player {
     if(player1) this.turn = true;
     this.selected = [];
     this.numbers = [1,2,3,4,5,6,7,8,9];
-    // this.won = false;
+    this.won = false;
   }
 
   getLength(){
     return this.numbers.length;
   }
 
-  removeNumber(number, isPlayer){
-    const num = parseInt(number);
+  removeNumber(id, isPlayer){
 
-    function removePick(num) {
-      let initNumbersLength = this.numbers.length;
-      let index = this.numbers.indexOf(num);
+    let num = parseInt(id);
+    let run = false;
 
-      if (index > -1) this.numbers.splice(index, 1);
-      if (this.numbers.length < initArrLength) return true;
-      return false;
-    }
+    if(!isPlayer) run = true;
 
-    if(!isPlayer){
-      let pick = removePick(num)
-      return pick;
-    }
-
-    if( typeof(index) == 'number' ){
-
-      let pick = removePick(num);
-      if (pick){
-        this.selected.push(num);
-        return true;
+    if(!run){
+      //run loop to check if id number is in array
+      for (var i = 0; i < this.numbers.length; i++){
+        if (i == num) {
+          run = true;
+        } else {
+          return false;
+        }
       }
-
-    } else {
-
-      return false;
-
     }
 
-
-
-
-
+    //if found remove+add to selected. if not return false
+    if(run){
+      let index = this.numbers.indexOf(num);
+      this.numbers.splice(index,1);
+      this.selected.push(num)
+      return true;
+    } else {
+      return false;
+    }
 
   }
 
   checkWin(){
-    let result = false;
+    // let result = false;
 
     //winning sequences
     const winSequences = [ [1,2,3], [4,5,6], [7,8,9], [7,4,1], [8,5,2], [9,6,3], [7,5,3], [1,5,9] ];
@@ -167,13 +164,13 @@ class Player {
 
         //check if 3 matches made
         if (count == 3){
-          result = true;
+          // result = true;
+          this.won = true;
         }
       }
     }
 
     // change this.won status
     // if(result) this.won = true;
-    return result;
   }//checkWin
 }//player class
